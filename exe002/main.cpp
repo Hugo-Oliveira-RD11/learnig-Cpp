db5c6e746747a0a78b3e1d2a4488e9ddc4d67c5d
Hugo de Oliveira Resende <hugo.resennde1123@gmail.com>
2023-01-21 15:11:14 -0300

    first exe

---
+++ exe002/main.cpp
@@ -0,0 +1,12 @@
+#include <cstdio>
+#include <iostream>
+
+using namespace std;
+int main() {
+  char name[100];
+  printf("digite o seu nome:");
+  scanf("%s", name);
+
+  printf("É um prazer te conhecer %s", name);
+  return 0;
+}
